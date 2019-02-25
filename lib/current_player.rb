53361763e179ab524e586cd07d6a2327122d77cd
mariah <mariah@gardziola.com>
2019-02-25 21:37:13 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,17 +1,9 @@
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-
-
-def turn_count(board)
-  
-  counter = 0 
-       
-   board.each do | turn | 
-
-     if turn == "X" || "O"
-       counter += 1 
-     end
-     
+def turn_count board
+  counter = 0
+  board.each do | turn |
+    if turn == "X" || turn == "O"
+      counter += 1
+    end
   end
-
-  return counter 
+  return counter
 end

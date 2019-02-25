fadaa04c66d742d8c7883b5397f0eee908211d03
mariah <mariah@gardziola.com>
2019-02-25 21:36:49 +0000

    Automatically backed up by Learn

---
+++ lib/current_player.rb
@@ -0,0 +1,17 @@
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+
+def turn_count(board)
+  
+  counter = 0 
+       
+   board.each do | turn | 
+
+     if turn == "X" || "O"
+       counter += 1 
+     end
+     
+  end
+
+  return counter 
+end
